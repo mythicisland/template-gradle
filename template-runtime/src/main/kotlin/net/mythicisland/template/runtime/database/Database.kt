9a52c92a52956c4ef1d98f961cd091b2006a405e
xXJanisXx <Janis.Minecrafto@gmx.de>
2026-02-09 10:39:43 +0100

    feat: basic service setup

---
+++ template-runtime/src/main/kotlin/net/mythicisland/template/runtime/database/Database.kt
@@ -0,0 +1,21 @@
+package net.mythicisland.template.runtime.database
+
+import org.jooq.impl.DSL
+import org.jooq.DSLContext
+
+class Database(
+    val context: DSLContext
+) {
+    fun setup() {
+        System.setProperty("org.jooq.no-logo", "true")
+        System.setProperty("org.jooq.no-tips", "true")
+        val setupInputStream = Database::class.java.getResourceAsStream("/schema.sql")
+            ?: throw IllegalArgumentException("Database schema not found.")
+        val setupCommands = setupInputStream.bufferedReader().use { it.readText() }.split(";")
+        setupCommands.forEach {
+            val trimmed = it.trim()
+            if (trimmed.isNotEmpty())
+                context.execute(DSL.sql(trimmed))
+        }
+    }
+}
---
+++ template-runtime/src/main/kotlin/net/mythicisland/template/runtime/database/DatabaseFactory.kt
@@ -0,0 +1,10 @@
+package net.mythicisland.template.runtime.database
+
+import org.jooq.impl.DSL
+
+object DatabaseFactory {
+    fun createDatabase(databaseUrl: String): Database {
+        val databaseContext = DSL.using(databaseUrl)
+        return Database(databaseContext)
+    }
+}
